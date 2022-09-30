0502cda9927040ba71a8f6233c7b623618039b83
Soroush Saffari <soroush@tabled.io>
2022-09-30 15:49:09 +0100

    Merge

--- src/app/router.js
+++ src/app/router.js
@@ -1,20 +1,29 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { PagePostCode } from '../pages';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { PagePostCode } from "../pages";
+import { Home } from "../pages/home";
+
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/'>
-          {/* <Home /> */}
+        <Route exact path="/">
+          <Home />
         </Route>
-        <Route exact path='/postcode'>
+        <Route exact path="/postcode">
           <PagePostCode />
         </Route>
-        <Route exact path='/message'></Route>
-        <Route exact path='/wtt'></Route>
-        <Route exact path='/done'></Route>
+        <Route exact path="/message">
+        </Route>
+        <Route exact path="/wtt">
+        </Route>
+        <Route exact path="/done">
+        </Route>
       </Switch>
     </BrowserRouter>
-  );
-};
+  )
+}
