5d56f1fa407edc1cff8e38b73fca3b9499bd0952
Pouya Jabbarisani <pouyajabbarisani@gmail.com>
2022-09-30 15:53:12 +0100

    Home completed

---
+++ src/pages/home/first-page-container.style.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+const FirstPageContainer = styled.main`
+	display: flex;
+	flex-direction: column;
+	flex-wrap: nowrap;
+	justify-content: center;
+	align-items: center;
+	align-content: center;
+	width: 100%;
+	height: 100vh;
+	background: #000;	
+	font-family: 'Poppins';
+	#home-page-intro{
+		display: flex;
+		flex-direction: column;
+		flex-wrap: nowrap;
+		justify-content: center;
+		align-items: stretch;
+		align-content: center;	
+		width: 20rem;
+		padding-bottom: 10vh;
+	}
+	h1{ 
+		color: #fff;
+		font-weight: 600;
+		text-align: center;
+		margin-bottom: 2rem;
+		font-size: 1.7rem;
+	}
+	.mahsa{
+		width: 12rem;
+		margin: 2rem auto 0 ;
+	}
+`;
+
+export default FirstPageContainer;
---
+++ src/pages/home/index.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import { Button } from "@mantine/core";
+import FirstPageContainer from "./first-page-container.style.js";
+import Mahsa from "./mahsa-grayscale.jpg";
+
+export const Home = () => {
+	return (
+		<FirstPageContainer>
+			<section id="home-page-intro">
+				<img src={Mahsa} alt="Mahsa Amini" className="mahsa" />
+				<h1>Show your support for <br/>#MahsaAmini</h1>
+				<Button variant="light" color="violet" radius="md" component={Link} to="/postcode">
+					Write to your PM
+				</Button>
+			</section>
+		</FirstPageContainer>
+	)
+}
