75f06749c14ea418ce36d6f1c4aa251394fb26e4
Pouya Jabbarisani <pouyajabbarisani@gmail.com>
2022-09-30 19:06:24 +0100

    fix bottom space of message page

--- src/app/app.js
+++ src/app/app.js
@@ -6,7 +6,7 @@ export const App = () => {
   return (
     <MantineProvider withNormalizeCSS>
       <GlobalStyle />
-      <Router />;
+      <Router />
     </MantineProvider>
   );
 };
--- src/pages/your-message/your-message.style.js
+++ src/pages/your-message/your-message.style.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 export const YourMessageContainer = styled.section`
+	display: flex;
+	flex-direction: column;
+	flex-wrap: nowrap;
+	justify-content: flex-start;
+	align-items: stretch;
 	position: relative;
 	width: 100%;
 	height: 100%;
@@ -8,9 +13,9 @@ export const YourMessageContainer = styled.section`
 		border: none;
 		border-radius: none;
 		width: 100%;
-		height: 100%;
+		flex-grow: 1;
+		flex-shrink: 0;
 		resize: none;
-		padding: 1rem;
 		font-family: 'Poppins', sans-serif;
 	}
 	.your-message__buttons{
@@ -21,8 +26,10 @@ export const YourMessageContainer = styled.section`
 		align-items: center;
 		align-content: center;	
 		width: 100%;
-		padding: 0.5rem 1rem;
+		padding: 0.5rem 0 0 0;
 		box-sizing: border-box;
+		flex-basis: 2.5rem;
+		flex-shrink: 0;
 		button{
 			height: 2rem !important;
 		}
